8ade8ddee6f85b9655c1b85f86abed5820acaf6b
LEE HYEONWOO <135234205+LEEHYEONWOOO@users.noreply.github.com>
2023-07-19 00:33:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,8 +3,9 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 	   List<String> ban_user = new ArrayList<String>();
-	   //report_map : 피신고자,set[신고자]
-	   Map<String,HashSet<String>> report_map = new HashMap<String,HashSet<String>>();//피신고자,신고자List
+        
+	   //report_map : 피신고자,HashSet[신고자]
+	   Map<String,HashSet<String>> report_map = new HashMap<String,HashSet<String>>();
 	   for(int i=0; i<report.length; i++) {
 		   HashSet<String> reporter_list = new HashSet<String>();
 		   if(report_map.get(report[i].split(" ")[1])==null) {
@@ -14,19 +15,16 @@ class Solution {
 			   report_map.get(report[i].split(" ")[1]).add(report[i].split(" ")[0]);
 		   }
 	   }
-	   //System.out.println(report_map.get("frodo"));
-	   
+	   //피신고 횟수가 k 이상이면 List<String> ban_user에 저장
 	   for(String user : report_map.keySet()) {
 		   if(report_map.get(user).size() >= k) {
 			   ban_user.add(user);
 		   }
 	   }
-	   System.out.println("ban_user : " + ban_user);
-	   
-	   for(String user : ban_user) {
-		   for(String reporter : report_map.get(user)) {
+	   //ban_user 객체에서 피신고자의 아이디로 report_map에 키값으로 넣어 신고자 이름 출력
+	   for(String user : ban_user) {    //ban_user 객체에서 피신고자의 아이디로
+		   for(String reporter : report_map.get(user)) {    //report_map에 키값으로 넣어 신고자 이름 출력
 			   answer[Arrays.asList(id_list).indexOf(reporter)] += 1;
-			   Arrays.asList(id_list).indexOf(reporter);
 		   }
 	   }
 	   return answer;
