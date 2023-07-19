2dfd0a8bace63c47f7e0b45e8c98e74f9a07f4de
LEE HYEONWOO <135234205+LEEHYEONWOOO@users.noreply.github.com>
2023-07-19 17:07:35 +0900

    Update 신규 아이디 추천.java

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,55 +2,33 @@ import java.util.*;
 class Solution {
     public String solution(String new_id) {
         String answer = "";
-        String special = "~!@#$%^&*()=+[{]}:?,<>/";
-        //1
-	   new_id = new_id.toLowerCase();
-	   //2
-	   for(int i=0; i<new_id.length(); i++) {
-		   if(special.contains(new_id.substring(i,i+1))) {
-			   new_id = new_id.replace(new_id.substring(i,i+1), "");
-			   i--;
-		   }
-	   }
-	   //3
-	   while(true) {
-		   if(new_id.contains("..")) {
-			   new_id = new_id.replace("..", ".");
-		   }else {
-			   break;
-		   }
-	   }
-	   //4
-       if(new_id.contains(".")) {
-	       if(new_id.substring(0,1).equals(".")) {
-               new_id = new_id.substring(1);
-	       }
-           if(new_id.length() >0){
-                if(new_id.charAt(new_id.length()-1)=='.') {
-		            new_id = new_id.substring(0,new_id.length()-1);
-	            }
-           }
-       }
+       
+       //1
+	   answer = new_id.toLowerCase();
+	   //2~4
+	   answer = answer.replaceAll("[^0-9a-z-_.]", "");
+	   answer = answer.replaceAll("[.]{2,}", ".");
+	   answer = answer.replaceAll("^[.]{1}", "");
+
 	   //5
-	   if(new_id.length()==0) {
-		   new_id = "a";
+	   if(answer.length()==0) {
+		   answer = "a";
 	   }
 	   //6
-	   if(new_id.length()>=16) {
-		   new_id = new_id.substring(0,15);
+	   if(answer.length()>=16) {
+		   answer = answer.substring(0,15);
 	   }
-       if(new_id.length() >0){
-            if(new_id.charAt(new_id.length()-1)=='.') {
-		            new_id = new_id.substring(0,new_id.length()-1);
-            }
-       }
+      if(answer.length() >0){
+           if(answer.charAt(answer.length()-1)=='.') {
+		            answer = answer.substring(0,answer.length()-1);
+           }
+      }
 	   //7
-	   if(new_id.length()<=2) {
-		   while(new_id.length()!=3) {
-			   new_id += new_id.charAt(new_id.length()-1);
+	   if(answer.length()<=2) {
+		   while(answer.length()!=3) {
+			   answer += answer.charAt(answer.length()-1);
 		   }
 	   }
-        answer = new_id;
         return answer;
     }
 }
